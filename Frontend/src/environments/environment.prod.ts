140bb7e2a8fdc42f7d8b580b7a928ee2f19e3227
1rithwik <reddyrrithwik@gmail.com>
2024-12-25 20:05:00 +0530

    URL's update

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiBackendUrl: 'https://example.com', // Example production URL
+    apiBackendUrl: 'https://spring-render-backend-3gif.onrender.com', // Example production URL
   };
   
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiBackendUrl: 'http://localhost:8080', // Development URL
+    apiBackendUrl: 'http://localhost:8080'// Development URL
   };
   
